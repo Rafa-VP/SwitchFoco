326ef44c9cc3942ef701bad83fdd5028862982a4
rafael.vaca <revaca@pucese.edu.ec>
2023-01-27 10:53:19 -0500

    Cambios varios...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Loader, Space, Switch, Title } from "@mantine/core"
+import { JsonInput, Loader, Space, Switch, Title } from "@mantine/core"
 import { useQuery } from "@tanstack/react-query"
 import { useState } from "react"
 import deta from "./api/deta"
@@ -7,40 +7,61 @@ import IconMoonStarts from "./icons/IconMoonStarts"
 import IconSun from "./icons/IconSun"
 
 function App() {
+  // ||========================== STATES ===========================||
   const [state, setState] = useState("off")
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+  const [response, setResponse] = useState("{ }")
 
-  function toggleCheckbox(element) {
-    var xhr = new XMLHttpRequest()
-    if (element.checked) {
-      setState("on")
-      xhr.open("GET", "https://p9lrcf.deta.dev/switch-foco?relay=on", true)
-    } else {
-      setState("off")
-      xhr.open("GET", "https://p9lrcf.deta.dev/switch-foco?relay=off", true)
-    }
-    xhr.send()
-  }
-
-  const { isLoading } = useQuery({
+  // ||========================== GET RELAY STATE ===========================||
+  const { isInitialLoading } = useQuery({
     queryKey: ["deta"],
     queryFn: async () => await (await deta.get("/get-switch")).data,
     onSuccess: (res) => setState(res),
   })
 
-  if (isLoading) return <Loader />
+  // ||========================== HANDLE CHANGE STATE ===========================||
+  function toggleCheckbox(element) {
+    try {
+      setIsButtonDisabled(true)
+      var xhr = new XMLHttpRequest()
+      if (element.checked) {
+        setState("on")
+        xhr.open("GET", "https://p9lrcf.deta.dev/switch-foco?relay=on", true)
+      } else {
+        setState("off")
+        xhr.open("GET", "https://p9lrcf.deta.dev/switch-foco?relay=off", true)
+      }
+      xhr.send()
+      xhr.onprogress = () => setIsButtonDisabled(false)
+    } catch (err) {
+      console.error({ error: err })
+    } finally {
+      xhr.onload = () => {
+        setResponse(xhr.response)
+        setState(JSON.parse(xhr.response).global_state)
+      }
+    }
+  }
 
+  // ||========================== RETURN LOADING ===========================||
+  if (isInitialLoading) return <Loader />
+
+  // ||========================== MAIN APP ===========================||
   return (
     <div>
       <Title color={"cyan"}>Relay Switch</Title>
       <Space h={"xl"} />
       <Switch
         size={"xl"}
-        onLabel={<IconSun />}
-        offLabel={<IconMoonStarts />}
+        onLabel={!isButtonDisabled ? <IconSun /> : "Loading..."}
+        offLabel={!isButtonDisabled ? <IconMoonStarts /> : "Loading..."}
         color={"cyan"}
         onChange={(e) => toggleCheckbox(e.target)}
         checked={state === "on" ? true : false}
+        disabled={isButtonDisabled}
       />
+      <Space h={"xl"} />
+      <JsonInput placeholder={response} disabled={true} />
     </div>
   )
 }
---
+++ src/icons/IconAlertCircle.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function IconAlertCircle() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="#2c3e50"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <circle cx="12" cy="12" r="9" />
+      <line x1="12" y1="8" x2="12" y2="12" />
+      <line x1="12" y1="16" x2="12.01" y2="16" />
+    </svg>
+  )
+}
