af406cf287d8d6f4cffe596745e9b041737607b7
Rafael <76231028+rafaelvp17@users.noreply.github.com>
2023-01-26 20:58:35 -0500

    Avanzando...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import { Group, Space, Switch, Title, useMantineTheme } from "@mantine/core"
+import { Loader, Space, Switch, Title } from "@mantine/core"
+import { useQuery } from "@tanstack/react-query"
 import { useState } from "react"
+import deta from "./api/deta"
 import "./App.css"
 import IconMoonStarts from "./icons/IconMoonStarts"
 import IconSun from "./icons/IconSun"
 
 function App() {
-	const theme = useMantineTheme()
-	const [timelapse, setTimeLapse] = useState(10)
+  const [state, setState] = useState("off")
 
   function toggleCheckbox(element) {
     var xhr = new XMLHttpRequest()
     if (element.checked) {
+      setState("on")
       xhr.open("GET", "https://p9lrcf.deta.dev/switch-foco?relay=on", true)
     } else {
+      setState("off")
       xhr.open("GET", "https://p9lrcf.deta.dev/switch-foco?relay=off", true)
     }
     xhr.send()
   }
+
+  const { isLoading, isFetching } = useQuery({
+    queryKey: ["deta"],
+    queryFn: async () => await (await deta.get("/get-switch")).data,
+    onSuccess: (res) => setState(res),
+    refetchInterval: 5000,
+    refetchIntervalInBackground: true,
+  })
+
+  if (isLoading) return <Loader />
 
   return (
     <div>
@@ -28,6 +41,10 @@ function App() {
         offLabel={<IconMoonStarts />}
         color={"cyan"}
         onChange={(e) => toggleCheckbox(e.target)}
+        checked={state === "on" ? true : false}
+        disabled={isFetching}
+        label={isFetching && "Refreshing..."}
+        labelPosition={"right"}
       />
     </div>
   )
---
+++ src/api/deta.js
@@ -0,0 +1,4 @@
+import axios from "axios"
+const deta = axios.create({ baseURL: "https://p9lrcf.deta.dev" })
+
+export default deta
